72aa2594f04499a8855576c043358f7834f363b3
STNeto1 <gfreitasneto18@gmail.com>
2023-04-22 21:47:53 -0300

    feat: very poor impl

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
 
 use anyhow::{Context, Result};
 use serde::Deserialize;
@@ -102,6 +102,59 @@ impl Database {
 
         Ok(())
     }
+
+    fn get_tf(&self, key: &u32, token: &str) -> f32 {
+        let top = self.tf.get(key).unwrap().get(token).clone().unwrap_or(&0);
+        let bottom = self.tf.get(key).unwrap().values().sum::<u32>();
+
+        return *top as f32 / bottom as f32;
+    }
+
+    fn get_idf(&self, token: &str) -> f32 {
+        let n = self.entries.len() as f32;
+        let df = self
+            .tf
+            .values()
+            .filter(|kv| kv.contains_key(token))
+            .count()
+            .max(1) as f32;
+
+        return (n / df).ln();
+    }
+
+    fn search(&self, query: &str) -> Result<Vec<(Entry, f32)>> {
+        let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None)
+            .map_err(|err| anyhow::anyhow!(err))?;
+
+        let encoding = tokenizer
+            .encode(query, false)
+            .map_err(|err| anyhow::anyhow!(err))?;
+
+        let tokens = encoding.get_tokens();
+
+        let mut result = Vec::<(u32, f32)>::new();
+        for (id, _kv) in self.tf.clone() {
+            let mut total_sum = 0f32;
+            for token in tokens {
+                let tf_rating = self.get_tf(&id, token);
+                let idf_rating = self.get_idf(token);
+
+                total_sum += tf_rating * idf_rating;
+            }
+
+            result.push((id, total_sum));
+        }
+
+        result.sort_by(|(_, a), (_, b)| b.partial_cmp(a).unwrap());
+
+        let mut entries = Vec::new();
+        for (id, score) in result {
+            let entry = self.entries.iter().find(|e| e.id == id).unwrap();
+            entries.push((entry.clone(), score));
+        }
+
+        Ok(entries)
+    }
 }
 
 fn main() -> Result<()> {
@@ -117,10 +170,22 @@ fn main() -> Result<()> {
             db.add(entry);
         });
 
+    let now = Instant::now();
     db.tokenize_entries()
         .context("Failed to tokenize entries")?;
+    println!("tokenize entries took {}ms", now.elapsed().as_millis());
 
-    println!("finished tokenizing {} entries", db.entries.len());
+    let term = "Green Smoothie";
+    println!("search for {}", term);
+
+    let now = Instant::now();
+    let search_result = db.search(term)?;
+    println!("search took {}ms", now.elapsed().as_millis());
+
+    search_result
+        .iter()
+        .take(10)
+        .for_each(|entry| println!("\t{} {} => {}", entry.0.id, entry.0.name, entry.1));
 
     Ok(())
 }
